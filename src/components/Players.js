fe897f763e64a6c22bfea4f7d14eec190a4cdf7d
MoAdetola <phamosche7@gmail.com>
2023-09-06 14:43:47 +0100

    props

---
+++ src/components/Players.js
@@ -0,0 +1,17 @@
+// src/players.js
+
+const players = [
+    {
+      name: "Marcus Rashford",
+      team: "Manchester United",
+      nationality: "England",
+      jerseyNumber: 10,
+      age: 25,
+      imageUrl: "https://example.com/messi.jpg",
+    },
+    // Add more players
+  ];
+  
+  
+  export default players;
+  
